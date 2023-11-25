5eae518f85eaf994a0888dcc9a5dc12742c1ad1c
semil338 <semilkhedawala@gmail.com>
2023-11-25 21:02:01 +0530

    header component done

--- next.config.js
+++ next.config.js
@@ -1,4 +1,9 @@
 /** @type {import('next').NextConfig} */
-const nextConfig = {}
+const nextConfig = {
+  images: {
+    domains: ["links.papareact.com"],
+  },
+  reactStrictMode: false,
+};
 
-module.exports = nextConfig
+module.exports = nextConfig;
--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,10 +1,11 @@
+import Board from "@/components/Board";
 import Header from "@/components/Header";
 
 export default function Home() {
   return (
     <main className="">
       <Header />
-      <h1>Main Board</h1>
+      <Board />
     </main>
   );
 }
