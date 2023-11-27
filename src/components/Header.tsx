51c7bcce9c9f6140788558660e145538d2f869c7
semil338 <semilkhedawala@gmail.com>
2023-11-27 22:58:14 +0530

    stop spinning

--- src/components/Header.tsx
+++ src/components/Header.tsx
@@ -69,9 +69,8 @@ const Header = () => {
       <div className="flex items-center justify-center px-5 py-2 md:py-5">
         <p className="flex items-center text-sm font-light p-5 pr-5 shadow-xl rounded-xl w-fit bg-white italic max-w-3xl text-[#0055D1]">
           <UserCircleIcon
-            className={`inline-block h-10 w-10 text-[#0055D1] mr-1 ${
-              loading && "animate-spin"
-            }`}
+            // ${loading && "animate-spin"}
+            className={`inline-block h-10 w-10 text-[#0055D1] mr-1 `}
           />
           {suggestion && !loading
             ? suggestion
